dc4e7454aad0ea8159fff24910e2242f63022a8c
Wang Han <66519887+happywind-boy@users.noreply.github.com>
2025-05-21 10:05:44 +0800

    Delete OMT/@Sphere directory

--- OMT/@Sphere/Folding.m
+++
@@ -1,7 +0,0 @@
-function [FoldingNum, FoldingInd] = Folding(F, S)
-CF = Tri.FaceCenter(F, S);
-CF = Vertex.Normalize(CF);
-NF = Tri.Normal(F, S);
-IP = dot(CF, NF, 2);
-FoldingInd = find(IP<0);
-FoldingNum = length(FoldingInd);
--- OMT/@Sphere/ProjGradVOMT.m
+++
@@ -1,8 +0,0 @@
-function [S, Bdry] = ProjGradVOMT(T, V, Bdry, VB, VI, Weight, V_Weight, doLineSearch)
-if ~exist('doLineSearch', 'var')
-    doLineSearch = 1;
-end
-Bdry = Sphere.AOMT_ProjGrad(Bdry, doLineSearch);
-S   = Sphere.WeightedHomotopy(T, V, Bdry, VB, VI, Weight, V_Weight);
-Vol = Tet.VertexVolume(T, V);
-S   = Vertex.RotOMT(S, V, Vol.*V_Weight);
